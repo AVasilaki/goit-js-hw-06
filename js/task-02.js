462aff95e1f4d0cecd56ca3101e1594f5d619ed3
AVasilaki <poseydon463@gmail.com>
2023-07-27 20:15:43 +0300

    Update task-02.js

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,14 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+const list = document.querySelector('#ingredients');
+
+const murkup = ingredients.map(element => {
+  const li = document.createElement('li');
+  console.log(li);
+  li.classList.add('item');
+  li.textContent = element;
+
+  return li;
+});
+list.append(...murkup);
