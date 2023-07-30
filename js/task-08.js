98746853df88bab01ffdc7c998e4d6636f992a0d
AVasilaki <poseydon463@gmail.com>
2023-07-30 09:11:19 +0300

    Update task-08.js

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,15 @@
+const form = document.querySelector('form');
+form.addEventListener('submit', handlerSubmit);
+function handlerSubmit(evt) {
+  evt.preventDefault();
 
+  const {
+    elements: { email, password },
+  } = evt.currentTarget;
+
+  if (email.value === '' || password.value === '') {
+    return alert('Please fill in all the fields!');
+  }
+  console.log(`email: ${email.value}, Password: ${password.value}`);
+  evt.currentTarget.reset();
+}
