2dbae2caabcad8ea9385af6bb54640262da17b85
AVasilaki <poseydon463@gmail.com>
2023-07-30 19:49:43 +0300

    Update task-10.js

--- js/task-10.js
+++ js/task-10.js
@@ -3,3 +3,30 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+const elements = {
+  input: document.querySelector('input'),
+  btnCreate: document.querySelector('button[data-create]'),
+  btnDestroy: document.querySelector('button[data-destroy]'),
+  div: document.querySelector('#boxes'),
+};
+function createBoxes(amount) {
+  for (let index = 0; index < amount; index++) {
+    const box = document.createElement('div');
+    let size = 30 + index * 10;
+    box.style.width = `${size}px`;
+    box.style.height = `${size}px`;
+    box.style.backgroundColor = `${getRandomHexColor()}`;
+    elements.div.append(box);
+  }
+}
+
+elements.btnCreate.addEventListener('click', handlerNumbers);
+function handlerNumbers() {
+  const numbers = elements.input.valueAsNumber;
+
+  createBoxes(numbers);
+}
+elements.btnDestroy.addEventListener('click', handlerDestroy);
+function handlerDestroy(evt) {
+  elements.div.innerHTML = '';
+}
